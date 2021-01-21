6cba13140f403d069da09872e0783a7897218dbe
selfnutmeg <step.gor00@icloud.com>
2021-01-21 21:02:02 +0300

    Выделил скрипт в отдельный файл, удалил закомментированный код

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+// Drop Menu
+
+let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+  // Map JS
+  let mapBlock = document.querySelector(".contacts__map");
+  mapBlock.classList.add("contacts__map-js");
